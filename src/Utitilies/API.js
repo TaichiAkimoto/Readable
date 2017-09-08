d2fd55311516ffdafe0cd1196b46d7424c4370b1
TaichiAkimoto <italyitalienitalia@gmail.com>
2017-09-08 14:54:27 +0900

    Add APIs

---
+++ src/Utitilies/API.js
@@ -0,0 +1,100 @@
+const api = "http://localhost:5001"
+
+// Generate a unique token for the backend server.
+let token = localStorage.token
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+  'Authorization': token
+}
+
+export const getCategories = () =>
+  fetch(`${api}/categories`, { headers })
+    .then(res = res.json())
+
+export const getPosts = () =>
+  fetch(`${api}/posts`, { headers })
+    .then(res = res.json())
+
+export const postAPost = (newPost) =>
+  fetch(`${api}/posts`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ newPost })
+  }).then(res => res.json())
+
+export const voteForAPost = (id, upOrDown) =>
+  fetch(`${api}/posts/${id}`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ upOrDown })
+  }).then(res => res.json())
+
+export const editAPost = (id, newPost) =>
+  fetch(`${api}/posts/${id}`, {
+    method: 'PUT',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ newPost })
+  }).then(res => res.json())
+
+export const deleteAPost = (id) =>
+  fetch(`${api}/posts/${id}`, {
+    method: 'DELETE',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+
+export const getComments = (id) =>
+  fetch(`${api}/posts/${id}/comments`, { headers })
+    .then(res = res.json())
+
+export const addAComment = ( newComment ) =>
+  fetch(`${api}/comments`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ newComment })
+  }).then(res => res.json())
+
+export const voteForAComment = (id, upOrDown) =>
+  fetch(`${api}/comments/${id}`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ upOrDown })
+  }).then(res => res.json())
+
+export const editAComment = (id, newComment) =>
+  fetch(`${api}/comments/${id}`, {
+    method: 'PUT',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ newComment })
+  }).then(res => res.json())
+
+export const deleteAComment = (id) =>
+  fetch(`${api}/comments/${id}`, {
+    method: 'DELETE',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
