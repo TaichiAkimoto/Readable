3ce1b1b8c5faa28cb3e1fd5189713d8addf853b6
TaichiAkimoto <italyitalienitalia@gmail.com>
2017-09-22 16:41:20 +0900

    Add react icon for alert

--- src/Components/create-edit.js
+++ src/Components/create-edit.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import serializeForm from 'form-serialize'
 import PropTypes from 'prop-types'
+import AlertContainer from 'react-alert'
+import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o'
 
 class CreateEdit extends Component {
   state = {
@@ -12,8 +14,30 @@ class CreateEdit extends Component {
   handleSubmit = (e) => {
     e.preventDefault()
     const values = serializeForm(e.target, { hash: true })
-    console.log(values);
-    this.props.onSubmitForm()
+    if(this.checkOptions(values)){
+      this.props.onSubmitForm()
+    }
+  }
+  checkOptions = (values) => {
+    if(values.title&&values.body&&values.author&&values.category)
+      return true
+    else
+      this.showAlert()
+      return false
+  }
+  alertOptions = {
+    offset: 14,
+    position: 'top center',
+    theme: 'dark',
+    time: 5000,
+    transition: 'scale'
+  }
+  showAlert = () => {
+    this.msg.show('empty!!', {
+      time: 2000,
+      type: 'success',
+      icon: <CalendarIcon size={50}/>
+    })
   }
   componentDidMount() {
     if(this.props.match.params.id){
@@ -24,6 +48,7 @@ class CreateEdit extends Component {
     const { create } = this.state
     return (
       <div className="App">
+        <AlertContainer ref={a => this.msg = a} {...this.alertOptions} />
         <form onSubmit={this.handleSubmit}>
           <div>
             <input type='text' name='title' placeholder='Title' /><br/>
